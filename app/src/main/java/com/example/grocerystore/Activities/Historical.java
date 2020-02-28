ba158648320937d539f777ad0ec19bb7bb1aa75c
Lawrence Oconnor <46226511+Lawrence-Oconnor@users.noreply.github.com>
2020-02-28 17:09:03 -0500

    -Creation of Decisions Activity on Historical

--- app/src/main/java/com/example/grocerystore/Activities/Historical.java
+++ app/src/main/java/com/example/grocerystore/Activities/Historical.java
@@ -2,9 +2,15 @@ package com.example.grocerystore.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.example.grocerystore.Dialogs.UserInputs;
+import com.example.grocerystore.Dialogs.popupMenu;
 import com.example.grocerystore.HelperClasses.Charts;
 import com.example.grocerystore.R;
 import com.github.mikephil.charting.charts.BarChart;
@@ -19,8 +25,9 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 
-public class Historical extends AppCompatActivity {
+public class Historical extends AppCompatActivity implements View.OnClickListener {
 
+    Button userInput;
 
     //Quadrant 1
     PieChart pieChartQ1A;
@@ -49,6 +56,11 @@ public class Historical extends AppCompatActivity {
 
     ArrayList<String> departments;
     ArrayList<String> foods;
+    ArrayList<String> stockLabels;
+    ArrayList<String> costLabels;
+    ArrayList<String> statusLabels;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,30 +85,30 @@ public class Historical extends AppCompatActivity {
     private void quadrant1() {
 
         pieChartQ1A = (PieChart) findViewById(R.id.pcQ1A);
-        pieChartQ1A = Charts.pieChart(pieChartQ1A);
+        pieChartQ1A = Charts.pieChart(pieChartQ1A , statusLabels);
 
         pieChartQ1B = (PieChart) findViewById(R.id.pcQ1B);
-        pieChartQ1B = Charts.pieChart(pieChartQ1B);
+        pieChartQ1B = Charts.pieChart(pieChartQ1B, statusLabels);
 
         pieChartQ1C = (PieChart) findViewById(R.id.pcQ1C);
-        pieChartQ1C = Charts.pieChart(pieChartQ1C);
+        pieChartQ1C = Charts.pieChart(pieChartQ1C, statusLabels);
 
         //top charts above
 
         pieChartQ1D = (PieChart) findViewById(R.id.pcQ1D);
-        pieChartQ1D = Charts.pieChart(pieChartQ1D);
+        pieChartQ1D = Charts.pieChart(pieChartQ1D, departments);
 
         pieChartQ1E = (PieChart) findViewById(R.id.pcQ1E);
-        pieChartQ1E = Charts.pieChart(pieChartQ1E);
+        pieChartQ1E = Charts.pieChart(pieChartQ1E, departments);
 
         pieChartQ1F = (PieChart) findViewById(R.id.pcQ1F);
-        pieChartQ1F = Charts.pieChart(pieChartQ1F);
+        pieChartQ1F = Charts.pieChart(pieChartQ1F, departments);
     }
 
     public void quadrant2() {
 
         pieChart1 = (PieChart) findViewById(R.id.chart1Pie);
-        pieChart1 = Charts.pieChart(pieChart1);
+        pieChart1 = Charts.pieChart(pieChart1, stockLabels);
 
         lineChart = (LineChart)findViewById(R.id.Q2LineChart);
         lineChart = Charts.lineChart(lineChart);
@@ -112,10 +124,10 @@ public class Historical extends AppCompatActivity {
     private void quadrant3() {
 
         pieChartQ3A = (PieChart) findViewById(R.id.pcQ3A);
-        pieChartQ3A = Charts.pieChart(pieChartQ3A);
+        pieChartQ3A = Charts.pieChart(pieChartQ3A, foods);
 
         pieChartQ3B = (PieChart) findViewById(R.id.pcQ3B);
-        pieChartQ3B = Charts.pieChart(pieChartQ3B);
+        pieChartQ3B = Charts.pieChart(pieChartQ3B, costLabels);
     }
 
     private void quadrant4() {
@@ -131,6 +143,25 @@ public class Historical extends AppCompatActivity {
     private void initlalValues() {
         foods = new ArrayList<>();
         departments = new ArrayList<>();
+        stockLabels = new ArrayList<>();
+        costLabels = new ArrayList<>();
+        statusLabels = new ArrayList<>();
+
+
+        stockLabels.add("FOH");
+        stockLabels.add("BOH");
+        stockLabels.add("EMPTY");
+
+        statusLabels.add("OffSite");
+        statusLabels.add("Idle");
+        statusLabels.add("Active");
+
+
+        costLabels.add("Delivery");
+        costLabels.add("Expiration Penalty");
+        costLabels.add("Employee Wayes");
+        costLabels.add("Product Costs");
+        costLabels.add("Inventory Penalty");
 
         foods.add("Apples");
         foods.add("Bananas");
@@ -149,7 +180,16 @@ public class Historical extends AppCompatActivity {
 
     }
 
+
     private void setupView() {
+        userInput = findViewById(R.id.btnInputs);
+        userInput.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        //Toast.makeText(getApplicationContext(),"Testing Button", Toast.LENGTH_SHORT).show();
+        Intent intent5 = new Intent(this, UserInputs.class);
+        startActivity(intent5);
+    }
 }
